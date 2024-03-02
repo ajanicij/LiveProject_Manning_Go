719772b3f858598e28d0b72dc154b78c555ed3d5
Aleksandar Janicijevic <aleks@vogonsoft.com>
2024-03-02 16:23:50 -0500

    Fibonacci

---
+++ 03-Recursion/02-Fibonacci/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"strconv"
+	"fmt"
+)
+
+func fibonacci(n int64) int64 {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+func main() {
+    for {
+        // Get n as a string.
+        var nString string
+        fmt.Printf("N: ")
+        fmt.Scanln(&nString)
+
+        // If the n string is blank, break out of the loop.
+        if len(nString) == 0 { break }
+
+        // Convert to int and calculate the Fibonacci number.
+        n, _ := strconv.ParseInt(nString, 10, 64)
+        fmt.Printf("fibonacci(%d) = %d\n", n, fibonacci(n))
+    }
+}
+
