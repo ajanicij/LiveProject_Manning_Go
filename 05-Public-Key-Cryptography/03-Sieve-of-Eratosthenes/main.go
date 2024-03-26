9ebdc09892f5bff65ac3139d847de06660f990c5
Aleksandar Janicijevic <aleks@vogonsoft.com>
2024-03-26 19:18:07 -0400

    Sieve of Eratosthenes

---
+++ 05-Public-Key-Cryptography/03-Sieve-of-Eratosthenes/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+// Build a sieve of Eratosthenes.
+func sieveOfEratosthenes(max int) []bool {
+	isPrime := make([]bool, max + 1)
+	for i := 2; i <= max; i++ {
+		isPrime[i] = true
+	}
+	for i := 2; i*i <= max; i++ {
+		if !isPrime[i] {
+			continue
+		}
+		for j := i*i; j <= max; j = j + i {
+			isPrime[j] = false
+		}
+	}
+	return isPrime
+}
+
+// Print out the primes in the sieve.
+func printSieve(sieve []bool) {
+	count := 1
+	fmt.Printf("%d ", 2)
+	for i := 3; i < len(sieve); i += 2 {
+		if sieve[i] {
+			fmt.Printf("%d ", i)
+			count++
+			if count == 10 {
+				count = 0
+				fmt.Println()
+			}
+		}
+	}
+	fmt.Println()
+}
+
+func main() {
+	max := 10000
+	sieve := sieveOfEratosthenes(max)
+	fmt.Printf("Primes up to %d:\n", max)
+	printSieve(sieve)
+}
